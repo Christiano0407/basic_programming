0194cd85c3d8d01df39701fda08bbf6bd876e3e1
Christiano0407 <placebo0704@gmail.com>
2022-08-23 12:00:25 -0500

    class_POO

---
+++ src/js/classMokepon.js
@@ -0,0 +1,33 @@
+//** === Prove POO (Programming Oriented Object) === */
+class Mokepon {
+  constructor(name, image, life, power) {
+    this.name = name;
+    this.image = image;
+    this.life = life;
+    this.power = power;
+  }
+}
+
+let monsterPower = new Mokepon(
+  `MonsterFire`,
+  `https://media.redadn.es/imagenes/pokemaster_338177_pn2.jpg`,
+  3,
+  65
+);
+
+let waterBombs = new Mokepon(
+  `WaterBomb`,
+  `https://alfabetajuega.com/hero/2019/03/Squirtle-Looking-Happy.jpg?width=1200&aspect_ratio=1200:631`,
+  3,
+  60
+);
+let punchEarths = new Mokepon(
+  `PunchEarth`,
+  `https://dam.smashmexico.com.mx/wp-content/uploads/2017/09/03144141/trend-una-arana-con-la-forma-de-pikachu-si-existe-cover.jpg`,
+  3,
+  70
+);
+console.log(monsterPower);
+console.log(monsterPower.name);
+console.log(waterBombs);
+console.log(punchEarths);
