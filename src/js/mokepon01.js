dfa0cc76ff2bf99fd8fec3d07ed0303f3a358ae1
Chris_Velázquez04 <placebo0704@gmail.com>
2022-08-26 06:02:26 +0000

    previous changes

--- src/js/mokepon01.js
+++ src/js/mokepon01.js
@@ -7,7 +7,7 @@ import { fireAttack } from "./mokeponAttack03.js";
 import { waterAttack } from "./mokeponAttack03.js";
 import { earthAttack } from "./mokeponAttack03.js"; */
 //** === Call POO === */
-import { myMokepons } from "./classMokepon.js";
+import { monsterFires, myMokepons } from "./classMokepon.js";
 //** === Call Btn === */
 const btnSelectMascot = document.getElementById("IdBtnSelect");
 const btnSelectYourMonster = document.getElementById(`idSelectMonster`);
@@ -19,9 +19,10 @@ const textAttackTwo = document.querySelector(`.textAttackTwo`);
 const attackImages = document.getElementById("secTwoAttack");
 //** === Checked & Select ===  */
 /* const mokeponChecked = document.getElementById(`idMokeponChecked`); */
-const btnFire = document.querySelector(`#btnMokeponFire`);
+/* const btnFire = document.querySelector(`#btnMokeponFire`);
 const btnWater = document.querySelector(`#btnMokeponWater`);
-const btnEarth = document.querySelector(`#btnMokeponEarth`);
+const btnEarth = document.querySelector(`#btnMokeponEarth`); */
+const containerList = document.getElementById(`idMokeponList`);
 //* ===  General Variable ===  */
 let playMonster = document.querySelector(`#playerMonster`); // Span del Player =
 let enemyMonster = document.querySelector("#enemyMonster");
@@ -30,6 +31,7 @@ let inputFire;
 let inputWater;
 let inputEarth;
 let nameMokeponPlayer;
+let allAttackMokepon;
 //let enemyAttack = random(1, 3); //Random
 let enemyAttack = random(0, myMokepons.length - 1); // POO => Mokepon
 
@@ -59,15 +61,15 @@ const checkedMokepon = () => {
     if (inputFire.checked) {
       playMonster.innerHTML = inputFire.id;
       nameMokeponPlayer = inputFire.id;
-      btnFire.addEventListener("click", extraAttacks);
+      extraAttacks(nameMokeponPlayer);
     } else if (inputWater.checked) {
       playMonster.innerHTML = inputWater.id;
       nameMokeponPlayer = inputWater.id;
-      btnWater.addEventListener("click", extraAttacks);
+      extraAttacks(nameMokeponPlayer);
     } else if (inputEarth.checked) {
       playMonster.innerHTML = inputEarth.id;
       nameMokeponPlayer = inputEarth.id;
-      btnEarth.addEventListener("click", extraAttacks);
+      extraAttacks(nameMokeponPlayer);
     } else {
       alert("Sorry!! Your Not Select One. Please. select your Mokepon");
     }
@@ -90,8 +92,8 @@ const enemySelectAdd = () => {
   enemyMonster.innerHTML = myMokepons[enemyAttack].name;
 };
 
-//*! === Attackas Player === */
-function extraAttacks() {
+//*! === Attacka Player === */
+function extraAttacks(nameMokeponPlayer) {
   let powerAttack;
   myMokepons.forEach((mokepon) => {
     if (nameMokeponPlayer === mokepon.name) {
@@ -100,7 +102,19 @@ function extraAttacks() {
       console.log("We have Error!");
     }
 
-    console.log(powerAttack);
+    //console.log(powerAttack);
+    allAttacks(powerAttack);
+  });
+}
+function allAttacks(powerAttack) {
+  powerAttack.forEach((attack) => {
+    allAttackMokepon = ` 
+    <button id=${attack.id} class="btn btnMokepon">
+            <p class="textMokepon">${attack.name}</p>
+    </button>
+    `;
+
+    containerList.innerHTML += allAttackMokepon;
   });
 }
 
