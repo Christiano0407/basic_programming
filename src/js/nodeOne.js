a5b6826469d29ffa2958c8ea75951136bb300550
Chris_Velázquez04 <placebo0704@gmail.com>
2022-09-08 16:42:16 +0000

    port server express js

--- src/js/nodeOne.js
+++ src/js/nodeOne.js
@@ -1,3 +1,12 @@
-console.log("Hello World!");
-console.log("My text of Course Node Js and VideoGames!!");
-console.log("Very Happy for Me!! Love programming!");
+//** === Call Express Js  Server === */
+const express = require("express");
+
+//** === Create App => Server === */
+const app = express();
+
+//** ===> Call Server and Output/port ===  */
+app.listen(8080, () => {
+  console.log("Hello World!");
+  console.log("My text of Course Node Js and VideoGames!!");
+  console.log("Very Happy for Me!! Love programming!");
+});
