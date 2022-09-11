dafc2b0fb16030895fdae2dd6d308897a4cd7951
Chris_Velázquez04 <placebo0704@gmail.com>
2022-09-11 16:48:44 +0000

    backend Game

---
+++ src/backend/backendGame.js
@@ -0,0 +1,10 @@
+//** === Backend Express Call Server ===  */
+const express = require("express");
+
+//** === Create App Backend Mokepon Game === */
+const app = express();
+
+//** === Call Server === */
+app.listen(8080, () => {
+  console.log("Hello!! Welcome Mokepon VideoGame!");
+});
--- src/js/nodeOne.js
+++ src/js/nodeOne.js
@@ -1,16 +1,17 @@
 //** === Call Express Js  Server === */
-const express = require("express");
+//const express = require("express");
 
 //** === Create App => Server === */
-const app = express();
+//const app = express();
 
-app.get("/", (req, res) => {
+//** === Response AND Request => Client === */
+/* app.get("/", (req, res) => {
   res.send("Hello Client! Welcome of World Web!");
-});
+}); */
 
 //** ===> Call Server and Output/port ===  */
-app.listen(8080, () => {
+/* app.listen(8080, () => {
   console.log("Hello World!");
   console.log("My text of Course Node Js and VideoGames!!");
   console.log("Very Happy for Me!! Love programming!");
-});
+}); */
