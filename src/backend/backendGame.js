0ae0fee03dfb3b5471fe009434e975735402e4a3
Chris_Velázquez04 <placebo0704@gmail.com>
2022-09-11 16:57:12 +0000

    backend game 01

--- src/backend/backendGame.js
+++ src/backend/backendGame.js
@@ -4,6 +4,11 @@ const express = require("express");
 //** === Create App Backend Mokepon Game === */
 const app = express();
 
+//*! === Call Request and Response === Client === */
+app.get("/", (req, res) => {
+  res.send("Welcome a Mokepon Game!");
+});
+
 //** === Call Server === */
 app.listen(8080, () => {
   console.log("Hello!! Welcome Mokepon VideoGame!");
