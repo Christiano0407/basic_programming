fdf8bad4e6c0242308ae9f4e7b681fc21c966d40
Chris_Velázquez04 <placebo0704@gmail.com>
2022-11-06 15:32:48 +0000

    methods

---
+++ methodArray/methodApp.js
@@ -0,0 +1,13 @@
+//** === === Arrays Methods Javascript === === */
+const names = [
+  "Mau",
+  "Pamme",
+  "Alma",
+  "Alexa",
+  "Adrián",
+  "Elizabeth",
+  "Natasha",
+  "Natalia",
+];
+//*? ==> ForEach() */
+names.forEach((name) => console.log(`Hello, I am ${name}`));
