a768e38c17bbf5e874ab2e5ba12fefa1e437e8e0
Chris_Velázquez04 <placebo0704@gmail.com>
2022-11-06 16:02:22 +0000

    methods

--- methodArray/methodApp.js
+++ methodArray/methodApp.js
@@ -1,4 +1,4 @@
-//** === === Arrays Methods Javascript === === */
+//** === === ==> Arrays Methods Javascript <== === === ==> Mutable & InMutable <== === === */
 const names = [
   "Mau",
   "Pamme",
@@ -9,5 +9,46 @@ const names = [
   "Natasha",
   "Natalia",
 ];
+
+const videoGames = [
+  {
+    videoGame: "Zelda",
+    company: "Nintendo",
+    level: "Expert",
+  },
+  {
+    videoGame: "Mario",
+    company: "Nintendo",
+    level: "Expert",
+  },
+  {
+    videoGame: "Crash",
+    company: "Sony",
+    level: "Expert",
+  },
+  {
+    videoGame: "Fifa Football",
+    company: "EASport",
+    level: "Expert",
+  },
+  {
+    videoGame: "Gears Of Wars",
+    company: "Microsoft",
+    level: "Expert",
+  },
+];
+
+let fullGames = [];
+//console.log(videoGames);
 //*? ==> ForEach() */
 names.forEach((name) => console.log(`Hello, I am ${name}`));
+
+videoGames.forEach((play) => console.log(play));
+videoGames.forEach((player) => {
+  fullGames.push(player);
+});
+console.log(`FullGames:`, fullGames);
+
+//*? ==> MAP() */
+names.map((friend) => console.log(`Friend: ${friend}`));
+//console.log(names);
