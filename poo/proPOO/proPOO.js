fa5b1e28fb3b7e72d328ad7933cf8c09f0a7e723
Chris_Velázquez04 <placebo0704@gmail.com>
2022-11-07 17:12:26 +0000

    entries object

--- poo/proPOO/proPOO.js
+++ poo/proPOO/proPOO.js
@@ -6,4 +6,35 @@ class Hello {
     return "Hello Sound!!";
   }
 }
+
+//*! === Object.Keys() === */
+const object = {
+  name: "Frank",
+  age: 25,
+  country: "Spain",
+};
+
+//*! === Object.getOwnPropertyNames() === */
+const people = {
+  name: "Alma",
+  country: "Germany",
+  age: 25
+};
+
+//*! === Object.entries() === */
+const person = {
+  name: "Tanya",
+  country: "British",
+  age: 35,
+};
+
+
+//**TODO === === === === Call === === === === */
+//*? Call Static */
 console.log(Hello.sound());
+//*? Call Object.Keys */
+console.log(Object.keys(object));
+//*? Call Object.getOwnPropertyNames() */
+console.log(Object.getOwnPropertyNames(people));
+//*? Call Object.entries() */
+console.log(Object.entries(person));
